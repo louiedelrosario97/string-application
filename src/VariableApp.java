789c1ec3cc9bb4bbbaa6fc6efa58f936d112ab1e
Louie D <louie.delrosario97@gmail.com>
2026-04-08 21:03:19 -0700

    Completed Exercise 2 step 3

---
+++ src/VariableApp.java
@@ -0,0 +1,53 @@
+public class VariableApp {
+    public static void main(String[] args) {
+        //Question 1: Declare a variable to store your favorite color
+        String favoriteColor = "purple";
+        System.out.println("Louie's favorite color is " + favoriteColor);
+
+        //Question 2: Declare a variable to store the year you started this class
+        int ClassStartYear = 2026;
+        System.out.println("We started the YUU program in " + ClassStartYear);
+
+        //Question 3:Declare a variable to store your middle initial
+        char MiddleInitial = 'A';
+        System.out.println("My middle initial is " + MiddleInitial);
+
+        //Question 4: Declare a variable to store whether you have pets (yes/no)
+        boolean DostThouHasPet = true;
+
+        if (DostThouHasPet) {
+            System.out.println("I do has me a pet");
+        } else {
+            System.out.println("My goldfish passed away so I am alone now");
+        }
+
+        //Question 5: Declare a variable with a nice message or saying
+        String niceMessage = ("eat a Snickers.");
+        System.out.println("When you don't feel like yourself, " + niceMessage);
+
+
+        //Step 2
+        //Declare and initialize variables
+
+        //1. The number of days in a week
+        int DaysInTheWeek = 7;
+        System.out.println("The number of days in a week is " + DaysInTheWeek);
+
+        //2. The price of coffee
+        double priceOfCoffee = 4.99;
+        System.out.println("These scam artists just charged me " + priceOfCoffee + " for burnt coffee. Shameless.");
+
+        //3. Your favorite letter
+        char favLetter = 'L';
+        System.out.println("My favorite letter is " + favLetter + " as in LOLOL.");
+
+        //4. Whether it's raining (true/false)
+        boolean isRaining = false;
+        System.out.println("Is it raining outside? " + isRaining);
+
+
+
+    }
+}
+
+
